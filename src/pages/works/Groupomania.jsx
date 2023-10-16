e179d06032a32957e06cfc733a78486d329c3fdd
ThomasdevLC <thomas.lecam@gmail.com>
2023-10-16 17:36:43 +0200

    groupomania page

--- src/pages/works/Groupomania.jsx
+++ src/pages/works/Groupomania.jsx
@@ -1,8 +1,30 @@
+import { useState, useRef } from "react";
+import WorksComponent from "../../components/works/WorksComponent";
+import video from "../../assets/videos/groupomania-video.mp4";
+
 const Groupomania = () => {
+  const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const toggleVideo = () => {
+    if (isPlaying) {
+      videoRef.current.pause();
+    } else {
+      videoRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
   return (
-    <div>
-      <p>Groupomania</p>
-    </div>
+    <section className="groupomania-container">
+      <WorksComponent />
+      <div className="container__video">
+        <button className="container__video__btn" onClick={toggleVideo}>
+          {isPlaying ? "ll Pause" : "► Play video"}
+        </button>
+        <video className="container__video__player" width="100%" src={video} type="video/mp4" loop ref={videoRef}></video>
+      </div>
+    </section>
   );
 };
 
