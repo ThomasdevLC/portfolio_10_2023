28c9eef0d09d3403d5fccd8c207b4795bf8bfa4f
ThomasdevLC <thomas.lecam@gmail.com>
2023-10-16 17:22:12 +0200

    farm page

--- src/pages/works/FermeRougeraie.jsx
+++ src/pages/works/FermeRougeraie.jsx
@@ -1,7 +1,15 @@
+import WorksComponent from "../../components/works/WorksComponent";
+import farmProducts from "../../assets/images/ferme_rougeraie_about.webp";
+import farmAbout from "../../assets/images/ferme_rougeraie_products.webp";
+
 const FermeRougeraie = () => {
   return (
-    <div>
-      <p>FermeRougeraie</p>
+    <div className="farm__container">
+      <WorksComponent />
+      <div className="farm__description">
+        <img className="farm__description__image-one" src={farmProducts} alt="image projet ferme de la rougeraie page a propos" />
+        <img className="farm__description__image-two" src={farmAbout} alt="image projet ferme de la rougeraie page produits" />
+      </div>
     </div>
   );
 };
