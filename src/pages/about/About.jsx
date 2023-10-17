165c9acf9ad202c5bbaab824e135314f0ccc5b5f
ThomasdevLC <thomas.lecam@gmail.com>
2023-10-17 12:40:52 +0200

    intro stack list

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -10,45 +10,55 @@ const About = () => {
   const { switchLang } = useContext(LangContext);
 
   return (
-    <div className="intro-main">
-      <div className="intro-container">
-        <div className="home__intro">
-          <div className="home__intro__pic">
-            <img src={pic} alt="photo" />
-          </div>
-          <h1 className="home__intro__name">THOMAS LE CAM</h1>
-
-          <h2 className="home__intro__position">
-            <p>{switchLang === "fr" ? homeFr.position : homeEn.position}</p>
-          </h2>
-          <div className="home__intro__text">
-            <p> {switchLang === "fr" ? homeFr.intro : homeEn.intro}</p>
-          </div>
-
-          <p className="home__intro__city">Montpellier</p>
-          <p className="home__intro__contact">
-            06 43 84 36 32 <br />
-            thomas.lecam@gmail.com
-          </p>
-          <div className="home__intro__socials">
-            <a href="https://www.linkedin.com/in/thomaslecam/" target="_blank" rel="noreferrer">
-              <i className="fa-brands fa-linkedin-in"></i>
-            </a>
-            <a href="https://github.com/ThomasdevLC" target="_blank" rel="noreferrer">
-              <i className="fa-brands fa-github"></i>
-            </a>
-            <a href="mailto:thomas.lecam@gmail.com" target="_blank" rel="noreferrer">
-              <i className="fa-regular fa-envelope"></i>
-            </a>
-          </div>
-
-          <div className="home__intro__cv">
-            {" "}
-            <a className="header__cvbox__cv " href={switchLang === "fr" ? cvfr : cven} target="_blank" rel="noreferrer">
-              {switchLang === "fr" ? "MON CV" : "MY CV"}
-            </a>
-          </div>
+    <div className="intro">
+      <div className="intro__left">
+        <div className="intro__left__pic">
+          <img src={pic} alt="photo" />
         </div>
+        <h1 className="intro__left__name">THOMAS LE CAM</h1>
+
+        <h2 className="intro__left__position">
+          <p>{switchLang === "fr" ? homeFr.position : homeEn.position}</p>
+        </h2>
+        <div className="intro__left__text">
+          <p> {switchLang === "fr" ? homeFr.intro : homeEn.intro}</p>
+        </div>
+
+        <p className="intro__left__city">Montpellier</p>
+        <p className="intro__left__contact">
+          06 43 84 36 32 <br />
+          thomas.lecam@gmail.com
+        </p>
+        <div className="intro__left__socials">
+          <a href="https://www.linkedin.com/in/thomaslecam/" target="_blank" rel="noreferrer">
+            <i className="fa-brands fa-linkedin-in"></i>
+          </a>
+          <a href="https://github.com/ThomasdevLC" target="_blank" rel="noreferrer">
+            <i className="fa-brands fa-github"></i>
+          </a>
+          <a href="mailto:thomas.lecam@gmail.com" target="_blank" rel="noreferrer">
+            <i className="fa-regular fa-envelope"></i>
+          </a>
+        </div>
+
+        <div className="intro__left__cv">
+          <a className="intro__left__cv__link" href={switchLang === "fr" ? cvfr : cven} target="_blank" rel="noreferrer">
+            {switchLang === "fr" ? "MON CV" : "MY CV"}
+          </a>
+        </div>
+      </div>
+
+      <div className="intro__right">
+        <p className="intro__right__stack">STACK</p>
+        <ul className="intro__right__stack__list">
+          <li>HTML5/CSS3</li>
+          <li>JAVASCRIPT</li>
+          <li>REACT</li>
+          <li>VUE</li>
+          <li>LSASS</li>
+          <li>RESPONSIVE DESIGN</li>
+          <li>GIT</li>
+        </ul>
       </div>
     </div>
   );
