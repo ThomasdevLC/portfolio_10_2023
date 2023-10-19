ec42a8e3c3a2ecfbc331211ab9d7c0fce5946e96
ThomasdevLC <thomas.lecam@gmail.com>
2023-10-19 14:47:39 +0200

    duration

--- src/utils/Transition.jsx
+++ src/utils/Transition.jsx
@@ -10,7 +10,7 @@ const Transition = (OgComponent) => {
         initial={{ scaleX: 1 }}
         animate={{ scaleX: 0 }}
         exit={{ scaleX: 0 }}
-        transition={{ duration: 1.2, ease: [0.22, 1, 0.36, 1] }}
+        transition={{ duration: 1.3, ease: [0.22, 1, 0.36, 1] }}
       >
         <div className="slide-out-inner"></div>
       </motion.div>
