5117c6eac0c72da049787e98733a446210a73ac4
ThomasdevLC <thomas.lecam@gmail.com>
2023-10-20 14:58:23 +0200

    handleMouseMove

--- src/components/home/MenuItem.jsx
+++ src/components/home/MenuItem.jsx
@@ -1,14 +1,61 @@
 /* eslint-disable react/prop-types */
+import { useRef, useEffect } from "react";
 
 import { NavLink } from "react-router-dom";
-const MenuItem = ({ project, index, imageWrapperRef, menuItemsRef }) => {
+const MenuItem = ({ project, index }) => {
+  const { gsap } = window;
+  const menuItems = useRef([]);
+  const imageWrapper = useRef([]);
+
+  useEffect(() => {
+    menuItems.current = menuItems.current.map((item) => item);
+    imageWrapper.current = imageWrapper.current.map((item) => item);
+
+    menuItems.current.forEach((menu, index) => {
+      menu.addEventListener("mouseenter", () => handleMouseEnter(index));
+      menu.addEventListener("mouseleave", () => handleMouseLeave(index));
+      menu.addEventListener("mousemove", () => handleMouseMove(index));
+    });
+  });
+
+  const handleMouseEnter = (index) => {
+    gsap.set(imageWrapper.current[index], {
+      scale: 1,
+      rotation: -15,
+    });
+
+    gsap.to(imageWrapper.current[index], { opacity: 1, scale: 1.2, yPercent: 0, rotation: 2 });
+  };
+
+  const handleMouseLeave = (index) => {
+    gsap.to(imageWrapper.current[index], {
+      opacity: 0,
+      yPercent: -50,
+      scale: 0.8,
+      rotation: -15,
+    });
+  };
+
+  const handleMouseMove = (index) => {
+    menuItems.current[index].addEventListener("mousemove", (event) => {
+      const x = event.clientX;
+
+      const imageWrapperBounds = imageWrapper.current[index].getBoundingClientRect();
+      const itembounds = menuItems.current[index].getBoundingClientRect();
+      gsap.to(imageWrapper.current[index], {
+        duration: 1.25,
+        x: Math.abs(x - itembounds.left) - imageWrapperBounds.width / 1.55,
+      });
+    });
+  };
+
   return (
     <div className="menu__item">
-      <div className="menu__item_image_wrapper" ref={(el) => (imageWrapperRef.current[index] = el)}>
+      <div className="menu__item_image_wrapper" ref={(el) => (imageWrapper.current[index] = el)}>
         <img className="menu__item_image_inner" src={project.image} alt={project.title} />
       </div>
       <NavLink to={`/${project.link}/${project.id}`}>
-        <p className="menu__item__name" ref={(el) => (menuItemsRef.current[index] = el)}>
+        <p className="menu__item__name" ref={(el) => (menuItems.current[index] = el)}>
           {project.title}
         </p>
       </NavLink>
--- src/components/home/WorksList.jsx
+++ src/components/home/WorksList.jsx
@@ -1,44 +1,11 @@
-import { useRef, useEffect } from "react";
 import worklist from "../../worklist/worklistData";
 import MenuItem from "./MenuItem";
 
 const WorksList = () => {
-  const { gsap } = window;
-  const menuItems = useRef([]);
-  const imageWrapper = useRef([]);
-
-  useEffect(() => {
-    menuItems.current = menuItems.current.map((item) => item);
-    imageWrapper.current = imageWrapper.current.map((item) => item);
-
-    menuItems.current.forEach((menu, index) => {
-      menu.addEventListener("mouseenter", () => handleMouseEnter(index));
-      menu.addEventListener("mouseleave", () => handleMouseLeave(index));
-    });
-  });
-
-  const handleMouseEnter = (index) => {
-    gsap.set(imageWrapper.current[index], {
-      scale: 1,
-      rotation: -15,
-    });
-
-    gsap.to(imageWrapper.current[index], { opacity: 1, scale: 1.2, yPercent: 0, rotation: 2 });
-  };
-
-  const handleMouseLeave = (index) => {
-    gsap.to(imageWrapper.current[index], {
-      opacity: 0,
-      yPercent: -50,
-      scale: 0.8,
-      rotation: -15,
-    });
-  };
-
   return (
     <div className="menu">
       {worklist.map((project, index) => (
-        <MenuItem key={project.id} project={project} index={index} imageWrapperRef={imageWrapper} menuItemsRef={menuItems} />
+        <MenuItem key={project.id} project={project} index={index} />
       ))}
     </div>
   );
